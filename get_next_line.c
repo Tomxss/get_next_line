ea1cfb729de051b001a3314e9ace9a6d408203a5
U-DESKTOP-5IRVEEM\theun <theunis1g1coetzee@gmail.com>
2019-06-13 20:29:19 +0200

    Test Approved

--- get_next_line.c
+++ get_next_line.c
@@ -28,3 +28,31 @@ static t_list	*get_file(t_list **file, int fd)
 	tmp = *file;
 	return (tmp);
 }
+
+int	get_next_line(const int fd, char **line)
+{
+	char		buf[BUFF_SIZE + 1];
+	static t_list	*file;
+	int		i;
+	int		ret;
+	t_list		*curr;
+
+	if ((fd < 0 || line == NULL || read(fd, buf, 0) < 0))
+		return (-1);	//ERROR
+	curr = get_file(&file, fd);
+	MALLCHECK((*line = ft_strnew(1)));
+	while ((ret = read(fd, buf, BUFF_SIZE)))
+	{
+		buf[ret] = '\0';
+		MALLCHECK((curr->content = ft_strjoin(curr->content, buf)));
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	if (ret < BUFF_SIZE && !ft_strlen(curr->content))
+		return (0);
+	i = ft_ccpy(line, curr->content, '\n');
+	(i < (int)ft_strlen(curr->content))
+		? curr->content += (i + 1)
+		: ft_strclr(curr->content);
+	return (1);
+}
