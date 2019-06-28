59b24d79ebebb2304cf190ec85df05b6999684ab
Theunis Coetzee <tcoetzee@c3r7s7.wethinkcode.co.za>
2019-06-28 08:54:01 +0200

    Update Pull Error

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tcoetzee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 12:39:54 by tcoetzee          #+#    #+#             */
-/*   Updated: 2019/06/27 15:47:53 by tcoetzee         ###   ########.fr       */
+/*   Updated: 2019/06/28 08:53:05 by tcoetzee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,6 @@ t_list	*get_curr(int fd, t_list **hlist)
 
 int		safe_read(const int fd, char **content)
 {
-<<<<<<< HEAD
 	int		res;
 	char	*tmp;
 	char	buf[BUFF_SIZE + 1];
@@ -58,26 +57,9 @@ int		safe_read(const int fd, char **content)
 		if (!(*content = ft_strjoin(*content, buf)))
 			return (-1);
 		free(tmp);
-=======
-	char		buf[BUFF_SIZE + 1];
-	static t_list	*file;	//declares a starting struct like *head
-	int		i;
-	int		ret;
-	t_list		*curr;	//declares a current struct
-
-	if ((fd < 0 || line == NULL || read(fd, buf, 0) < 0))	// 0 = read
-		return (-1);	//ERROR				// 1 = write
-	curr = get_file(&file, fd);// recieves the completed struct// 2 = read
-	CHECK((*line = ft_strnew(1)));			// 4 = read-write
-	while ((ret = read(fd, buf, BUFF_SIZE)))
-	{
-		buf[ret] = '\0';
-		CHECK((curr->content = ft_strjoin(curr->content, buf)));
->>>>>>> 884442cfd53ad1a3d71863b2a41a4b94e1f984f3
 		if (ft_strchr(buf, '\n'))
 			break;
 	}
-<<<<<<< HEAD
 	return (res);
 }
 
@@ -107,13 +89,4 @@ int		get_next_line(const int fd, char **line)
 	else
 		tmp[0] = '\0';
 	return (1);
-=======
-	if (ret < BUFF_SIZE && !ft_strlen(curr->content))
-		return (0);	//completed
-	i = ft_ccpy(line, curr->content, '\n');
-	(i < (int)ft_strlen(curr->content))
-		? curr->content += (i + 1)
-		: ft_strclr(curr->content);
-	return (1);//has been read
->>>>>>> 884442cfd53ad1a3d71863b2a41a4b94e1f984f3
 }
